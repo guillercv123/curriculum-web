f31c38719ea1fc00383ed5ef4d4df22820394ae1
guillercv123 <guillercv123@gmail.com>
2025-01-10 13:31:48 -0500

    udpate para produccion

--- src/components/experience/experience.component.ts
+++ src/components/experience/experience.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {PdfModalComponent} from "../../common/pdf-modal-component/pdf-modal-component.component";
 import {MatDialog} from "@angular/material/dialog";
+import {environment} from "../../environments/environment";
 
 @Component({
   selector: 'app-experience',
@@ -8,12 +9,13 @@ import {MatDialog} from "@angular/material/dialog";
   styleUrl: './experience.component.css'
 })
 export class ExperienceComponent {
-
+  url: string = '';
   constructor(private dialog: MatDialog) {}
+
   openPdfModal(){
     this.dialog.open(PdfModalComponent, {
       data: {
-        pdfSrc: '/assets/pdf/cv.pdf'  // Asegúrate de que esta ruta sea correcta
+        pdfSrc: environment.production ?'/curriculum-web/assets/pdf/cv.pdf'  :  '/assets/pdf/cv.pdf'
       },
       width: '80%',
       height: '80%',
